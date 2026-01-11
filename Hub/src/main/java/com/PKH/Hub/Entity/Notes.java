d162d730196e8b087dc4f2b57609a21146f79b14
suryaa-developer <suryaarul2001@gmail.com>
2026-01-11 09:47:03 +0530

    Feat : added entity for notes

--- Hub/src/main/java/com/PKH/Hub/Entity/Notes.java
+++ Hub/src/main/java/com/PKH/Hub/Entity/Notes.java
@@ -1,4 +1,30 @@
 package com.PKH.Hub.Entity;
 
-public class Notes {
-}
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.NonNull;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+ public class Notes {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "noteid")
+    private int id;
+
+    @NotBlank(message =  "Title should not be Empty")
+    private String title;
+    @Column(length = 1000)
+    private String Description;
+    private LocalDateTime CreatedAt;
+    private LocalDateTime UpdatedAt;
+
+    @NotNull(message = "User Must be provided")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id",nullable = false)
+    private  User user;
+ }
